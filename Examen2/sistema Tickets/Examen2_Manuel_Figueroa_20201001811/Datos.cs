b001c60cdfca1f6dcaa3e5bdf6c14b190042e369
Manullet <mdfigueroa@unah.hn>
2022-11-03 23:33:53 -0600

    examen2
    inicio

---
+++ Examen2/sistema Tickets/Examen2_Manuel_Figueroa_20201001811/Datos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen2_Manuel_Figueroa_20201001811
+{
+    internal class Datos
+    {
+    }
+}
---
+++ Examen2/sistema Tickets/Examen2_Manuel_Figueroa_20201001811/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Examen2_Manuel_Figueroa_20201001811
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
